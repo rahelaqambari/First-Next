5b5589f4626f64defd2b0c7262f7a4224fb70486
rahelaqambari <rahelaqambari@gmail.com>
2025-12-14 19:53:26 +0430

    H1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,17 @@ export default function Home() {
         <h1 className='bg-linear-to-r from-cyan-800 to-red-900 via-indigo-700 bg-clip-text text-transparent text-6xl font-bold'><span className="text-2xl text-black font-medium mb-32">const <span className="text-2xl text-indigo-700 font-medium mb-32 ">name</span> = </span> "Rahela Meysam"</h1>
         <h4 className="text-base font-bold"><span className="text-indigo-800 font-bold text-base ">console.log(</span>"Hey, dear Rahela"<span className="text-indigo-700 font-bold text-base ">);</span></h4>
       </div> */}
-      <div className="h-4 rounded-2xl relative w-[90%] bg-stone-200 ">
+      {/* <div className="h-4 rounded-2xl relative w-[90%] bg-stone-200 ">
         <div className="bg-green-700 h-6 rounded-full relative left-48 bottom-1 w-6">
         </div>
         <div className="h-28 absolute left-[201px] w-1.5 bg-green-700 top-4"></div>
         <div className="h-64 w-64 rounded-full border-8 border-t-green-700 border-b-green-600 border-r-green-600 border-l-green-700 absolute top-32 left-[71px]"></div>
-      </div>
+      </div> */}
 
-      
+      <div className="h-fit w-fit p-50 text-base">
+        <h1>IN the Name of ALLAH</h1>
+
+      </div>
     </div>
   );
 }
