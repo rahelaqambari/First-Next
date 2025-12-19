403ff2f7e3ae4aabb40400a998e831c84efbd3ca
rahelaqambari <rahelaqambari@gmail.com>
2025-12-19 21:47:59 +0430

    style is added

--- src/app/count/Counter.tsx
+++ src/app/count/Counter.tsx
@@ -4,17 +4,12 @@
 function Counter() {
     const [value ,setvalue] = useState (0);
   return (
-    <div style={{
-        backgroundColor : "blue",
-        width: "50%",
-        height: "40%",
-        borderRadius: "8px",
-        alignItems: "center",
-        color: "white"
-    }}>
-      <h1>The number is  {value}</h1>
-      <button onClick={() => setvalue(value +1)}>incermant</button>
-      <button onClick={() => setvalue(value -1)}>decremant</button>
+    <div className='h-[50vh] w-[60%] rounded-md border-2 mx-auto my-20 flex flex-col justify-center items-center'>
+      <h1 className='text-5xl'>The number is <span className='text-5xl font-bold text-cyan-800'>{value}</span></h1>
+      <div className='flex justify-between w-[50%] mt-8'>
+        <button className='bg-amber-800 text-2xl text-white py-3 rounded-md px-2' onClick={() => setvalue(value +1)}>incermant</button>
+      <button className='bg-cyan-800 text-2xl text-white py-3 rounded-md px-2' onClick={() => setvalue(value -1)}>decremant</button>
+      </div>
     </div>
   );
 }
