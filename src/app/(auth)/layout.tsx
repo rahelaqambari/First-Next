6e566f45c2673b8b5f3b04bbf418ba37631fce2b
rahelaqambari <rahelaqambari@gmail.com>
2025-12-23 21:11:25 +0430

    Login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,17 +4,15 @@ function layout({childern}: {children : React .ReactNode}) {
   return (
     <html>
         <body>
-            <nav style={{
-            padding:' 6px',
-            backgroundColor: 'cyan',
-            color:"white",
-        }}>header</nav>
-        {childern}
-        <footer style={{
-            padding:' 6px',
-            backgroundColor: 'cyan',
-            color:"white",
-        }}>Footer</footer>
+            <div className='h-screen w-full flex items-center justify-center'>
+            <div className='h-[70%] w-6/12 border rounded-2xl flex flex-col justify-center items-center'>
+            <h1 className='text-6xl font-bold text-center'>Login</h1>
+            <input type="text" placeholder='Enter your Name' className='border outline-0 py-4 px-4 w-10/12 rounded-full m-2 mt-12 hover:shadow-2xl shadow-emerald-900' />
+            <input type="text" placeholder='Enter your Email' className='border outline-0 py-4 px-4 w-10/12 rounded-full m-2 hover:shadow-2xl shadow-emerald-900' />
+            <input type="text" placeholder='Enter your Password' className='border outline-0 py-4 px-4 w-10/12 rounded-full m-2 hover:shadow-2xl shadow-emerald-900' />
+            <button className='py-4 px-6 text-2xl font-bold bg-green-900 text-white rounded-2xl hover:cursor-pointer'>Login</button>
+            </div>
+            </div>
         </body>
     </html>
   )
