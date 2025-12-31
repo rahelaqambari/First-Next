b39ea71740bb10b6c3b538b52359bc4cfda67097
rahelaqambari <rahelaqambari@gmail.com>
2025-12-31 19:06:58 +0430

    suspense

---
+++ src/Components/Load.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Load() {
+  return (
+    <div className='border border-b-0 rounded-full animate-spin h-12 w-12'>
+      
+    </div>
+  )
+}
+
+export default Load
--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,7 +1,8 @@
+import Load from '@/Components/Load'
 import Pest from '@/Components/Pest'
 import Review from '@/Components/Review'
 import { Metadata } from 'next'
-import React from 'react'
+import React, { Suspense } from 'react'
 export const metadata :Metadata = {
   title: "contact",
   description: "About part",
@@ -11,9 +12,13 @@ function page() {
   return (
    <div className='h-screen w-full flex flex-col items-center justify-center'>
     <h1 className='text-green-700 font-bold p-12 text-6xl'>Contact page</h1>
-      <div className='text-3xl items-center justify-center flex p-24'>
+      <div className='text-3xl gap-6 items-center justify-center flex p-24'>
+      <Suspense fallback={ <Load />}>
         <Pest/>
-      <Review/>
+      </Suspense>
+      <Suspense fallback={<h5>Loading...</h5>}>
+        <Review/>
+      </Suspense>
       </div>
       
     </div>
