c77af1cec742abfce3e25e5c599c39f29e004cc7
rahelaqambari <rahelaqambari@gmail.com>
2026-01-04 22:53:01 +0430

    changes

--- src/app/cliented/page.tsx
+++ src/app/cliented/page.tsx
@@ -1,19 +1,39 @@
 "use client";
 import { useTheme } from '@/Components/theme-provider'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 function page() {
+    // const Theme = useTheme();
+    // const [dark ,setdark] = useState(Theme.colors.primary);
+    // useEffect(()=>{
+    //     setdark("#00ff00");
+    // },[setdark])
+    // function handlecolore(){
+    //     setdark("#0000ff");
+    //     }
     const Theme = useTheme();
     const [dark ,setdark] = useState(false);
-    function handelcolore(){
-        Theme.colors.primary = "#00ff00";
-    }
   return (
-         <div className="h-10/12 mt-32 w-full items-center justify-center flex-col flex"> 
-      <h1>Hi There , Good Evening</h1>
-      <button className="bg-green-600 py-4 px-5 rounded-md shadow-2xs shadow-white text-center" onClick={()=>handelcolore(setdark!)}>Toggle theme</button>
+         <div className="h-screen w-full items-center justify-center flex-col flex" style={dark?
+            {
+                color:"green", 
+              
+
+            }:
+            {
+                color:"black",
+             
+              
+            }
+        }> 
+      <h1 className='text-4xl font-bold'>Hi There , Good Evening</h1>
+      <button className="bg-green-600 py-4 px-5 rounded-md shadow-2xs shadow-white text-center" onClick={()=>setdark(!dark)}>Toggle theme</button>
     </div>
   )
 }
 
 export default page
+
+
+    
+ 
