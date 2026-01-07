d8f9c34632a9abd8cbd7f3f23ed1bd5c8ea36e2e
rahelaqambari <rahelaqambari@gmail.com>
2026-01-07 07:37:46 +0430

    error page

---
+++ src/app/serverData/error.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='h-80 w-full flex items-center justify-center'>
+        <div className='h-56 w-56 border border-b-0'>
+            <h1 className='text-8xl'>❌</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default loading
--- src/app/serverData/page.tsx
+++ src/app/serverData/page.tsx
@@ -1,6 +1,4 @@
 import Link from 'next/link';
-import React from 'react'
-
 type Client = {
     id: number;
     name: string;
