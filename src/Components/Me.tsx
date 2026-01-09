1cb03de3fad271f50fe1598f129544071227cc9d
rahelaqambari <rahelaqambari@gmail.com>
2026-01-09 21:01:58 +0430

    style

--- src/Components/Me.tsx
+++ src/Components/Me.tsx
@@ -1,11 +1,8 @@
-import { imageConfigDefault } from "next/dist/shared/lib/image-config"
-import Image from "next/image"
-
  function Me() {
   return (
-    <div className="h-screen w-full grid grid-cols-2 items-center justify-center ">
-      <div>
-        <h1>Salam , Iam Rahela Qambari A Web Desinger </h1>
+    <div className="h-screen w-full grid grid-cols-2 items-center justify-center gap-6 bg-linear-30 from-blue-900 to-white/45">
+      <div className="flex items-center justify-center flex-col text-justify p-12 text-white">
+        <h1 className="text-2xl font-bold">Salam , Iam <span className="text-3xl bg-linear-30 from-blue-950 to-cyan-900 bg-clip-text text-transparent">Rahela Qambari</span> A Web Desinger </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ad incidunt quis molestiae voluptatem asperiores aliquid saepe, perferendis sit aspernatur quaerat assumenda autem placeat tenetur praesentium, at ea, impedit dolor?</p>
       </div>
       <div>
---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,12 @@
+import Me from '@/Components/Me'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Me />
+    </div>
+  )
+}
+
+export default page
