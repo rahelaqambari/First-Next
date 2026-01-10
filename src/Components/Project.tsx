5a2fbdd0a7fb74592543e82947940f2ceadfc589
rahelaqambari <rahelaqambari@gmail.com>
2026-01-10 22:17:19 +0430

    style

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -4,9 +4,11 @@ function Project() {
   return (
     <div className='flex flex-col items-center justify-center gap-2 bg-linear-30 from-white/30 to-blue-800'>
         <h1 className='text-4xl font-black text-white p-12'>Projects</h1>
-      <div className='h-fit w-4/12 border rounded-md p-12 border-white'>
-
+        <div className='w-full flex items-center justify-center gap-3'>
+            <div className='h-fit w-4/12 border rounded-md p-12 border-white flex flex-col justify-center items-center'>
+            <h1 className='text-2xl font-bold text-white p-12'>Dantal Clinic</h1>
       </div>
+        </div>
     </div>
   )
 }
