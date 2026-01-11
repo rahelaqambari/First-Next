307c72eb064e5454dff22d788aa8fec5deb25ee6
rahelaqambari <rahelaqambari@gmail.com>
2026-01-11 21:04:16 +0430

    fetching albums data

--- src/app/parallel/page.tsx
+++ src/app/parallel/page.tsx
@@ -11,13 +11,17 @@ type album = {
     tittle: string
 };
 async function getposts(id:string):Promise<Post[]>{
-     const response =  await fetch(`https://jsonplaceholder.typicode.com/posts`);
+     const response =  await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+     return response.json();
+}
+async function getalbum(id:string):Promise<Post[]>{
+     const response =  await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${id}`);
      return response.json();
 }
 export default async function page({params,}:{params: Promise<{id:string}>;}) {
     const {id} = await params;
     const postdata = getposts(id);
-    // const albumdata = getalbum(id);
+    const albumdata = getalbum(id);
     const [posts,albums] = await Promise.all([postdata,albumdata]);
     return(
     <div className="w-full grid grid-cols-2 gap-3">
