f7d3948a4cd6a375e5c883430b9931406747cbd0
rahelaqambari <rahelaqambari@gmail.com>
2026-01-11 21:10:46 +0430

    ui post

--- src/app/parallel/page.tsx
+++ src/app/parallel/page.tsx
@@ -24,6 +24,16 @@ export default async function page({params,}:{params: Promise<{id:string}>;}) {
     const albumdata = getalbum(id);
     const [posts,albums] = await Promise.all([postdata,albumdata]);
     return(
-    <div className="w-full grid grid-cols-2 gap-3">
+    <div className="w-full bg-stone-800 grid grid-cols-2 gap-3">
+        {/* posts */}
+        {posts.map(Post=>(
+            <div key={Post.id} className="bg-stone-50 p-4 rounded-3xl">
+                <h1 className="text-2 text-green-700">{Post.tittle}</h1>
+                <p>{Post.body}</p>
+            </div>
+        ))}
+        {/* albums */}
+        {albums.map(Album=>(
+        ))}
     </div>)
 }
