444e6c9e36bac12f559292dfdd233271d97ac158
rahelaqambari <rahelaqambari@gmail.com>
2026-01-11 21:15:16 +0430

    loding page

---
+++ src/app/parallel/loading.tsx
@@ -0,0 +1,10 @@
+function loading() {
+  return (
+    <div className='h-80 w-full flex items-center justify-center'>
+        <div className='h-24 w-24 rounded-full border border-b-0 animate-spin'></div>
+      
+    </div>
+  )
+}
+
+export default loading
