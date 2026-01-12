74b416b406185ca8b59f7cd0f73d940c9f4a5f83
rahelaqambari <rahelaqambari@gmail.com>
2026-01-12 21:19:12 +0430

    p

--- src/app/articles/[articlesid]/page.tsx
+++ src/app/articles/[articlesid]/page.tsx
@@ -4,6 +4,7 @@ async function page({params}:{params: Promise<{articlesid: string}>}) {
     const articlesid = (await params).articlesid
   return (
     <div className='flex items-center justify-center w-full'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nihil itaque accusantium nemo amet consectetur iste molestias. Ad quidem repellat animi aliquid nam deleniti, nesciunt quae, vel, distinctio quibusdam voluptatum?</p>
      <h1 className='text-4xl font-bold p-4'> It is the Article <span className='text-5xl font-bold text-green-800 pt-12 animate-pulse'>{articlesid}</span></h1>
     </div>
   )
